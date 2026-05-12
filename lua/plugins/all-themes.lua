106ccbd4b97378e8ff2c355b8063f10c1404af81
dilshodturab <avlomberdioglid@gmail.com>
2026-05-12 18:04:27 +0500

    so default colorscheme added file name changed

--- lua/plugins/all-themes.lua
+++ lua/plugins/all-themes.lua
@@ -1,81 +1,24 @@
 return {
   -- Load all theme plugins but don't apply them
   -- This ensures all colorschemes are available for hot-reloading
-  {
-    "ribru17/bamboo.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "bjarneo/aether.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "bjarneo/ethereal.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "bjarneo/hackerman.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "sainnhe/everforest",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "kepano/flexoki-neovim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "luisiacc/gruvbox-baby",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "tahayvr/matteblack.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "loctvl842/monokai-pro.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "shaunsingh/nord.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    priority = 1000,
-  },
+  { "ribru17/bamboo.nvim", lazy = true, priority = 1000 },
+  { "cpea2506/one_monokai.nvim", lazy = true, priority = 1000 },
+  { "metalelf0/jellybeans-nvim", lazy = true, priority = 1000 },
+  { "rktjmp/lush.nvim", lazy = true, priority = 1000 },
+  { "rockyzhang24/arctic.nvim", lazy = true, priority = 1000 },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  { "bjarneo/aether.nvim", lazy = true, priority = 1000 },
+  { "bjarneo/ethereal.nvim", lazy = true, priority = 1000 },
+  { "bjarneo/hackerman.nvim", lazy = true, priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", lazy = true, priority = 1000 },
+  { "sainnhe/everforest", lazy = true, priority = 1000 },
+  { "kepano/flexoki-neovim", lazy = true, priority = 1000 },
+  { "ellisonleao/gruvbox.nvim", lazy = true, priority = 1000 },
+  { "luisiacc/gruvbox-baby", lazy = true, priority = 1000 },
+  { "rebelot/kanagawa.nvim", lazy = true, priority = 1000 },
+  { "tahayvr/matteblack.nvim", lazy = true, priority = 1000 },
+  { "loctvl842/monokai-pro.nvim", lazy = true, priority = 1000 },
+  { "shaunsingh/nord.nvim", lazy = true, priority = 1000 },
+  { "rose-pine/neovim", name = "rose-pine", lazy = true, priority = 1000 },
+  { "folke/tokyonight.nvim", lazy = true, priority = 1000 },
 }
