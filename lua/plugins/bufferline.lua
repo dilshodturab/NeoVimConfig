b7cc3e195c2985b9d7cc75f3d6a2981051cfb447
dilshodturab <avlomberdioglid@gmail.com>
2026-05-21 03:03:21 +0500

    bufferline is changed

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,7 +2,31 @@ return {
   "akinsho/bufferline.nvim",
   opts = {
     options = {
-      move_wraps_at_ends = false, -- optional
+      move_wraps_at_ends = false,
+      numbers = "ordinal", -- chap tomondagi raqamlar
+      indicator = {
+        icon = "▎",
+        style = "icon",
+      },
+      show_buffer_close_icons = true,
+      show_close_icon = false,
+      diagnostics = "nvim_lsp", -- LSP ikonkalar
+      diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or " "
+        return icon .. count
+      end,
+      left_trunc_marker = "",
+      right_trunc_marker = "",
+    },
+    highlights = {
+      buffer_selected = {
+        bold = true,
+        italic = true,
+        fg = "#7aa2f7",
+      },
+      indicator_selected = {
+        fg = "#7aa2f7",
+      },
     },
   },
   keys = {
