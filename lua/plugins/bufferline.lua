fbf218670806da05e7b9eb572d0a170aa0eaea1b
dilshodturab <avlomberdioglid@gmail.com>
2026-05-21 17:49:52 +0500

    bufferline change

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,18 +3,12 @@ return {
   opts = {
     options = {
       move_wraps_at_ends = false,
-      numbers = "ordinal", -- chap tomondagi raqamlar
       indicator = {
         icon = "▎",
         style = "icon",
       },
-      show_buffer_close_icons = true,
+      show_buffer_close_icons = false,
       show_close_icon = false,
-      diagnostics = "nvim_lsp", -- LSP ikonkalar
-      diagnostics_indicator = function(count, level)
-        local icon = level:match("error") and " " or " "
-        return icon .. count
-      end,
       left_trunc_marker = "",
       right_trunc_marker = "",
     },
